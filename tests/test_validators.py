f0b166b35a0100e1893e0c1360d6e7e7447bb1c2
Hiroki KIYOHARA <hirokiky@gmail.com>
2014-03-25 21:28:00 +0900

    Added a test for validator

---
+++ tests/test_validators.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+class TestValidateResource(unittest.TestCase):
+    def _callFUT(self, *args, **kwargs):
+        from karmaid.validators import validate_resource
+        return validate_resource(*args, **kwargs)
+
+    def test__not_str(self):
+        self.assertFalse(self._callFUT(100))
+
+    def test__too_short(self):
+        self.assertFalse(self._callFUT(''))
+
+    def test__too_long(self):
+        self.assertFalse(self._callFUT('a' * 5001))
+
+    def test__valid_short(self):
+        self.assertTrue(self._callFUT('a'))
+
+    def test__valid_long(self):
+        self.assertTrue(self._callFUT('a' * 5000))
