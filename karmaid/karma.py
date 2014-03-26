c1975dc805ac806ccb2ff2e85d6dcea63e7010d9
Hiroki KIYOHARA <hirokiky@gmail.com>
2014-03-26 21:21:04 +0900

    Fixed way to store karma:
    * To be able to make ranking

--- karmaid/karma.py
+++ karmaid/karma.py
@@ -1,18 +1,25 @@
 from karmaid.redisio import get_redis
 
 
-def get_karma_key(resource):
-    return 'karma__{}'.format(resource)
+KARMA_KEY_NAME = 'karma'
 
 
 def get_karma(resource):
-    karma = get_redis().get(get_karma_key(resource))
+    karma = get_redis().zscore(KARMA_KEY_NAME, resource)
     return int(karma) if karma else 0
 
 
 def inc_karma(resource):
-    return get_redis().incr(get_karma_key(resource))
+    return get_redis().zincrby(KARMA_KEY_NAME, resource)
 
 
 def dec_karma(resource):
-    return get_redis().decr(get_karma_key(resource))
+    return get_redis().zincrby(KARMA_KEY_NAME, resource, amount=-1)
+
+
+def get_best_resources():
+    return [r.decode() for r in get_redis().zrevrange(KARMA_KEY_NAME, 0, 9)]
+
+
+def get_worst_resources():
+    return [r.decode() for r in reversed(get_redis().zrevrange(KARMA_KEY_NAME, -10, -1))]
--- tests/test_karma.py
+++ tests/test_karma.py
@@ -1,23 +1,26 @@
 import unittest
 
+from karmaid.karma import KARMA_KEY_NAME
+from karmaid.redisio import get_redis
+
 
 def setUpModule():
     from karmaid.redisio import init_fake_redis
     init_fake_redis()
 
 
-class TestGetKarma(unittest.TestCase):
+class _RedisTestCase(unittest.TestCase):
+    def tearDown(self):
+        get_redis().flushall()
+
+
+class TestGetKarma(_RedisTestCase):
     def _callFUT(self, *args, **kwargs):
         from karmaid.karma import get_karma
         return get_karma(*args, **kwargs)
 
-    def tearDown(self):
-        from karmaid.redisio import get_redis
-        get_redis().flushall()
-
     def test__karma_existed(self):
-        from karmaid.redisio import get_redis
-        get_redis().set('karma__RitsuTainaka', 3)
+        get_redis().zadd(KARMA_KEY_NAME, 3, 'RitsuTainaka')
         actual = self._callFUT('RitsuTainaka')
         self.assertEqual(3, actual)
 
@@ -26,18 +29,13 @@ class TestGetKarma(unittest.TestCase):
         self.assertEqual(0, actual)
 
 
-class TestIncKarma(unittest.TestCase):
+class TestIncKarma(_RedisTestCase):
     def _callFUT(self, *args, **kwargs):
         from karmaid.karma import inc_karma
         return inc_karma(*args, **kwargs)
 
-    def tearDown(self):
-        from karmaid.redisio import get_redis
-        get_redis().flushall()
-
     def test__karma_existed(self):
-        from karmaid.redisio import get_redis
-        get_redis().set('karma__RitsuTainaka', 3)
+        get_redis().zadd(KARMA_KEY_NAME, 3, 'RitsuTainaka')
         actual = self._callFUT('RitsuTainaka')
         self.assertEqual(4, actual)
 
@@ -46,21 +44,80 @@ class TestIncKarma(unittest.TestCase):
         self.assertEqual(1, actual)
 
 
-class TestDecKarma(unittest.TestCase):
+class TestDecKarma(_RedisTestCase):
     def _callFUT(self, *args, **kwargs):
         from karmaid.karma import dec_karma
         return dec_karma(*args, **kwargs)
 
-    def tearDown(self):
-        from karmaid.redisio import get_redis
-        get_redis().flushall()
-
     def test__karma_existed(self):
-        from karmaid.redisio import get_redis
-        get_redis().set('karma__RitsuTainaka', 3)
+        get_redis().zadd(KARMA_KEY_NAME, 3, 'RitsuTainaka')
         actual = self._callFUT('RitsuTainaka')
         self.assertEqual(2, actual)
 
     def test__no_karma(self):
         actual = self._callFUT('RitsuTainaka')
         self.assertEqual(-1, actual)
+
+
+class TestGetBestResources(_RedisTestCase):
+    def _callFUT(self):
+        from karmaid.karma import get_best_resources
+        return get_best_resources()
+
+    def test__not_enough_resources(self):
+        redis = get_redis()
+        redis.zadd(KARMA_KEY_NAME, 10000, 'cureblack')
+        redis.zadd(KARMA_KEY_NAME, 9999, 'curesunny')
+        redis.zadd(KARMA_KEY_NAME, 1, 'curemarine')
+        actual = self._callFUT()
+        self.assertEqual(['cureblack', 'curesunny', 'curemarine'], actual)
+
+    def test__enough_resources(self):
+        redis = get_redis()
+        redis.zadd(KARMA_KEY_NAME, 6, 'ritsu')
+        redis.zadd(KARMA_KEY_NAME, 5, 'jun')
+        redis.zadd(KARMA_KEY_NAME, 4, 'ui')
+        redis.zadd(KARMA_KEY_NAME, 3, 'nodoka')
+        redis.zadd(KARMA_KEY_NAME, 2, 'ichigo')
+        redis.zadd(KARMA_KEY_NAME, 1, 'sawa-chan')
+        redis.zadd(KARMA_KEY_NAME, 0, 'ton-chan')
+        redis.zadd(KARMA_KEY_NAME, -1, 'azunyan')
+        redis.zadd(KARMA_KEY_NAME, -2, 'mugi')
+        redis.zadd(KARMA_KEY_NAME, -3, 'mio')
+        redis.zadd(KARMA_KEY_NAME, -4, 'yui')
+        actual = self._callFUT()
+        self.assertEqual(['ritsu', 'jun', 'ui', 'nodoka', 'ichigo',
+                          'sawa-chan', 'ton-chan', 'azunyan', 'mugi',
+                          'mio'], actual)
+
+
+class TestGetWorstResources(_RedisTestCase):
+    def _callFUT(self):
+        from karmaid.karma import get_worst_resources
+        return get_worst_resources()
+
+    def test__not_enough_resources(self):
+        redis = get_redis()
+        redis.zadd(KARMA_KEY_NAME, 10000, 'darkprettycure')
+        redis.zadd(KARMA_KEY_NAME, 9999, 'regine')
+        redis.zadd(KARMA_KEY_NAME, -100, 'akao-ni')
+        actual = self._callFUT()
+        self.assertEqual(['akao-ni', 'regine', 'darkprettycure'], actual)
+
+    def test__enough_resources(self):
+        redis = get_redis()
+        redis.zadd(KARMA_KEY_NAME, 100, 'beer')
+        redis.zadd(KARMA_KEY_NAME, 99, 'sake')
+        redis.zadd(KARMA_KEY_NAME, 98, 'chu-hai')
+        redis.zadd(KARMA_KEY_NAME, 97, 'shochu')
+        redis.zadd(KARMA_KEY_NAME, 96, 'whiskey')
+        redis.zadd(KARMA_KEY_NAME, 95, 'gin')
+        redis.zadd(KARMA_KEY_NAME, 94, 'rum')
+        redis.zadd(KARMA_KEY_NAME, 93, 'tequila')
+        redis.zadd(KARMA_KEY_NAME, 92, 'brandy')
+        redis.zadd(KARMA_KEY_NAME, 91, 'wine')
+        redis.zadd(KARMA_KEY_NAME, 90, 'grappa')
+        actual = self._callFUT()
+        self.assertEqual(['grappa', 'wine', 'brandy', 'tequila',
+                          'rum', 'gin', 'whiskey', 'shochu', 'chu-hai',
+                          'sake'], actual)
