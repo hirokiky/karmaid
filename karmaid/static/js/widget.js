a56641f902c693ed1ce8647b3851b190e563c76d
Hiroki KIYOHARA <hirokiky@gmail.com>
2014-03-28 20:24:12 +0900

    Fixed to get stuff from user inputs

--- karmaid/static/js/widget.js
+++ karmaid/static/js/widget.js
@@ -1,8 +1,7 @@
 (function (){
     var base_url = 'http://127.0.0.1:6543';
-    var stuff = 'hirokiky';
+    var stuff = karmaid_stuff;
     var write_string = '<iframe src="' + base_url + '/button/1/?stuff=' + stuff + '" height="23" width="' + (110 + stuff.length * 7).toString() + '" scrolling="no" frameborder="0" title="Karmaid Karma Button"></iframe>';
 
     document.write(write_string);
 })();
-
